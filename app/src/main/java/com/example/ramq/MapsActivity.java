421c84332de3876223791fb7f8cc7188627b0e22
Akshit <guliaa@mcmaster.ca>
2023-03-30 13:12:32 -0400

    Akshit's first commit

--- app/src/main/java/com/example/ramq/MapsActivity.java
+++ app/src/main/java/com/example/ramq/MapsActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.ramq.databinding.ActivityMapsBinding;
-
+//Akshit Comment
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
